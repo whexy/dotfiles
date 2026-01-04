ca9dd778b9deedeac4c957def3fcb2f24d45965a
Wenxuan Shi <whexy@outlook.com>
2026-01-03 22:16:54 -0600

    [hosts][remote-dev] add ssh key

--- hosts/remote-dev/default.nix
+++ hosts/remote-dev/default.nix
@@ -42,6 +42,9 @@ nixpkgs.lib.nixosSystem {
             "networkmanager"
             "docker"
           ];
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdMvHl7VPzajwBjWw+pqcLatA42yWtQKiEPj/9VqI9i"
+          ];
         };
 
         programs.zsh.enable = true;
