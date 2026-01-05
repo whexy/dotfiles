8a2d4e0f7bc40ae717858e6f94ae3812a665f9c5
Wenxuan Shi <wenxuan.shi@northwestern.edu>
2026-01-05 14:46:08 -0600

    [macos] add ssh keys

--- hosts/mbp/default.nix
+++ hosts/mbp/default.nix
@@ -24,8 +24,12 @@ nix-darwin.lib.darwinSystem {
 
         # User configuration
         users.users.${username} = {
+          description = username;
           home = "/Users/${username}";
           shell = pkgs.zsh;
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdMvHl7VPzajwBjWw+pqcLatA42yWtQKiEPj/9VqI9i"
+          ];
         };
 
         home-manager.useGlobalPkgs = true;
