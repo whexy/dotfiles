d6cf261f8b21f36be6743fa37f9b269edf6f43d7
Wenxuan Shi <whexy@outlook.com>
2026-02-19 19:54:21 -0600

    NixOS: enable mosh

--- system/base-nixos.nix
+++ system/base-nixos.nix
@@ -4,6 +4,9 @@
   # Enable zsh at system level (required for user shell)
   programs.zsh.enable = true;
 
+  # Enable mosh (mobile shell, ssh replacement)
+  programs.mosh.enable = true;
+
   # enable envFS (shabang)
   # disable envFS for WSL, because Windows expect /bin/mount exists
   services.envfs.enable = lib.mkIf (!wsl) true;
