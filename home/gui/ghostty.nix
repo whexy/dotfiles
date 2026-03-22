b1c36c98c677f53450069e7056a0d6183e951452
Wenxuan Shi <whexy@outlook.com>
2026-03-21 21:40:50 -0500

    ghostty: update configs
    remove unused configs since we are using window managers now.
    increase transparent to 10%.

--- home/gui/ghostty.nix
+++ home/gui/ghostty.nix
@@ -12,12 +12,8 @@ in
       theme = "Gruvbox Dark";
       font-size = 14;
       font-family = "FiraCode Nerd Font";
-      background-opacity = 0.95;
+      background-opacity = 0.90;
       background-blur = true;
-      window-width = 160;
-      window-height = 48;
-      window-position-x = 320;
-      window-position-y = 144;
       macos-option-as-alt = "left";
       keybind = [
         "alt+left=unbind"
