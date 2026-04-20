714d4e2a6c3b4675b47ce98ae29b32f78bbf7489
Wenxuan Shi <whexy@outlook.com>
2026-04-20 16:39:57 -0500

    darwin: force prometheus node exporter home
    This is a workaround mentioned in the upper stream issue.
    https://github.com/nix-darwin/nix-darwin/issues/1256

--- system/dev-darwin.nix
+++ system/dev-darwin.nix
@@ -36,4 +36,8 @@
     listenAddress = "0.0.0.0";
     port = 9100;
   };
+
+  # Work around nix-darwin string comparison: the existing system user has
+  # /private/var/… but the module defaults to /var/… (a symlink on macOS).
+  users.users._prometheus-node-exporter.home = lib.mkForce "/private/var/lib/prometheus-node-exporter";
 }
