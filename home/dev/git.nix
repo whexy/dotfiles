257106e9216769fd327d76e79ee7ffbcf22fead1
Wenxuan Shi <whexy@outlook.com>
2026-05-21 10:55:28 -0500

    dev: enable git LFS plugin

--- home/dev/git.nix
+++ home/dev/git.nix
@@ -17,6 +17,7 @@
 
   programs.git = {
     enable = true;
+    lfs.enable = true;
 
     signing = {
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIcI4E3boeSWD5+eb9K6Zotw7dxjjvHP60tBjoM0uYn";
