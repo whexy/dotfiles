7dd031b19278bd33730338ec79d5c97da50276f7
Wenxuan Shi <whexy@outlook.com>
2026-07-02 13:30:12 -0500

    Add jj

--- nix/modules/home/dev/git.nix
+++ nix/modules/home/dev/git.nix
@@ -15,47 +15,59 @@
     '';
   };
 
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
 
-    signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIcI4E3boeSWD5+eb9K6Zotw7dxjjvHP60tBjoM0uYn";
-      signByDefault = true;
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIcI4E3boeSWD5+eb9K6Zotw7dxjjvHP60tBjoM0uYn";
+        signByDefault = true;
+      };
+
+      settings = {
+        user = {
+          name = "Wenxuan Shi";
+          email = "whexy@outlook.com";
+        };
+
+        gpg = {
+          format = "ssh";
+          ssh = {
+            allowedSignersFile = "~/.git_allowed_signers";
+          }
+          // lib.optionalAttrs pkgs.stdenv.isDarwin {
+            program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+          };
+        };
+
+        credential = {
+          "https://github.com" = {
+            helper = "!${pkgs.gh}/bin/gh auth git-credential";
+          };
+          "https://gist.github.com" = {
+            helper = "!${pkgs.gh}/bin/gh auth git-credential";
+          };
+        };
+      };
     };
 
-    settings = {
-      user = {
-        name = "Wenxuan Shi";
-        email = "whexy@outlook.com";
-      };
-
-      gpg = {
-        format = "ssh";
-        ssh = {
-          allowedSignersFile = "~/.git_allowed_signers";
-        }
-        // lib.optionalAttrs pkgs.stdenv.isDarwin {
-          program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-        };
-      };
-
-      credential = {
-        "https://github.com" = {
-          helper = "!${pkgs.gh}/bin/gh auth git-credential";
-        };
-        "https://gist.github.com" = {
-          helper = "!${pkgs.gh}/bin/gh auth git-credential";
-        };
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        side-by-side = true;
       };
     };
-  };
 
-  programs.delta = {
-    enable = true;
-    options = {
-      line-numbers = true;
-      side-by-side = true;
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Wenxuan Shi";
+          email = "whexy@outlook.com";
+        };
+      };
     };
   };
 }
