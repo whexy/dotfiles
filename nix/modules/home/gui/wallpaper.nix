9c2ef038e16f7e04a2bf3f6f4e8cf73c2079deae
Wenxuan Shi <whexy@outlook.com>
2026-07-07 20:40:35 -0500

    Change wallpaper

--- nix/modules/home/gui/wallpaper.nix
+++ nix/modules/home/gui/wallpaper.nix
@@ -5,6 +5,12 @@
   darwin ? false,
   ...
 }:
+let
+  wallpaper = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/thatmechguy/Nix-wallpapers/b189654e49d7124ec0a3367a07b3a65c976886fe/custom%20p-b.png";
+    hash = "sha256-U3ByfTa9g8GANj/x0lcDueuvTZmyKoSegdZwFXp7bZQ=";
+  };
+in
 {
   # Linux: swaybg paints the wallpaper; spawned by niri at startup.
   home.packages = lib.optionals (!darwin) [ pkgs.swaybg ];
@@ -15,7 +21,7 @@
         command = [
           "swaybg"
           "--image"
-          "${./wallpaper.jpg}"
+          "${wallpaper}"
           "--mode"
           "fill"
         ];
@@ -26,7 +32,7 @@
   # macOS: tell Finder to set the desktop picture on each activation.
   home.activation = lib.optionalAttrs darwin {
     setWallpaper = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      /usr/bin/osascript -e 'tell application "Finder" to set desktop picture to POSIX file "${./wallpaper.jpg}"'
+      /usr/bin/osascript -e 'tell application "Finder" to set desktop picture to POSIX file "${wallpaper}"'
     '';
   };
 }
