aba85c91a4cbae810045eb28e7a8cb63cc0df031
Wenxuan Shi <whexy@outlook.com>
2026-08-10 07:54:50 -0500

    wsl: improve Nvidia GPU support

--- nix/hosts/wsl/nvidia.nix
+++ nix/hosts/wsl/nvidia.nix
@@ -2,17 +2,10 @@
 # https://yomaq.github.io/posts/nvidia-on-nixos-wsl-ollama-up-24-7-on-your-gaming-pc/
 { pkgs, ... }:
 {
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = true;
-
   environment.sessionVariables = {
     CUDA_PATH = "${pkgs.cudatoolkit}";
-    EXTRA_LDFLAGS = "-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib";
-    EXTRA_CCFLAGS = "-I/usr/include";
     LD_LIBRARY_PATH = [
       "/usr/lib/wsl/lib"
-      "${pkgs.linuxPackages.nvidia_x11}/lib"
-      "${pkgs.ncurses5}/lib"
     ];
     MESA_D3D12_DEFAULT_ADAPTER_NAME = "Nvidia";
   };
@@ -20,6 +13,7 @@
   hardware.nvidia-container-toolkit = {
     enable = true;
     mount-nvidia-executables = false;
+    suppressNvidiaDriverAssertion = true;
   };
 
   systemd.services = {
