3c70d4c53b7e3262f9189120c482b07375aedeb8
Wenxuan Shi <whexy@outlook.com>
2026-08-11 18:22:56 -0500

    doordash: update

--- nix/packages/doordash-cli/default.nix
+++ nix/packages/doordash-cli/default.nix
@@ -3,20 +3,34 @@
   ...
 }:
 
+let
+  platform =
+    {
+      aarch64-darwin = "darwin-arm64";
+      x86_64-linux = "linux-amd64";
+    }
+    .${pkgs.stdenv.hostPlatform.system}
+      or (throw "doordash-cli: unsupported system ${pkgs.stdenv.hostPlatform.system}");
+
+  hashes = {
+    darwin-arm64 = "sha256-TEZ81zquMZu9bZQOzta0gy2ZGThW6bAiFNXomJnM4A8=";
+    linux-amd64 = "sha256-G+gDmI5Bo/Twk9+A4OpQZZQN2oNDxWUoTiax1aj9KJw=";
+  };
+in
 pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "doordash-cli";
-  version = "0.2.0";
+  version = "0.2.2";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/doordash-oss/doordash-cli/releases/download/v${finalAttrs.version}/dd-cli-v${finalAttrs.version}-darwin-arm64.tar.gz";
-    hash = "sha256-zWUCwXBNErfX6bZNxY/afvrBp3Zafg64hyYPen3MZEI=";
+    url = "https://github.com/doordash-oss/doordash-cli/releases/download/v${finalAttrs.version}/dd-cli-v${finalAttrs.version}-${platform}.tar.gz";
+    hash = hashes.${platform};
   };
 
-  sourceRoot = "dd-cli-v${finalAttrs.version}-darwin-arm64";
+  sourceRoot = "dd-cli-v${finalAttrs.version}-${platform}";
 
   installPhase = ''
     runHook preInstall
-    install -Dm755 dd-cli-v${finalAttrs.version}-darwin-arm64 $out/bin/dd-cli
+    install -Dm755 dd-cli-v${finalAttrs.version}-${platform} $out/bin/dd-cli
     runHook postInstall
   '';
 
@@ -31,7 +45,10 @@ pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
     downloadPage = "https://github.com/doordash-oss/doordash-cli/releases";
     license = licenses.unfree;
     mainProgram = "dd-cli";
-    platforms = [ "aarch64-darwin" ];
+    platforms = [
+      "aarch64-darwin"
+      "x86_64-linux"
+    ];
     sourceProvenance = [ sourceTypes.binaryNativeCode ];
   };
 })
