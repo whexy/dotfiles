c9cbfd06aafd23817fd532b1821b44972c55fa00
Wenxuan Shi <wenxuan.shi@northwestern.edu>
2026-08-21 14:28:40 -0500

    b3srv0 root: profile init

---
+++ nix/hosts/b3srv0/users/root/home-configuration.nix
@@ -0,0 +1,29 @@
+{ inputs, config, ... }:
+{
+  imports = [
+    inputs.self.homeModules.all
+  ]
+  ++ inputs.self.lib.homeCapsModules [
+    "base"
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [
+    inputs.self.lib.overlays.unstable
+    inputs.self.lib.overlays.llm-tools
+  ];
+  dotfiles = {
+    autoUpgrade.enable = true;
+    agents = {
+      enable = true;
+      enableApiAccounts = true;
+      enableProxyAccounts = true;
+    };
+  };
+
+  age.identityPaths = [ "${config.home.homeDirectory}/.config/agenix/key.txt" ];
+
+  targets.genericLinux.enable = true;
+  targets.genericLinux.gpu.enable = false;
+  home.homeDirectory = "/root";
+}
