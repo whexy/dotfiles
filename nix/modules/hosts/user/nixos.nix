e1da795332ec04f824c670e6069cbf47205ccaf6
Wenxuan Shi <whexy@outlook.com>
2026-09-10 20:12:33 -0500

    feat(user): add me to dialout group

--- nix/modules/hosts/user/nixos.nix
+++ nix/modules/hosts/user/nixos.nix
@@ -22,6 +22,7 @@ in
         "networkmanager"
         "docker"
         "incus-admin"
+        "dialout"
       ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdMvHl7VPzajwBjWw+pqcLatA42yWtQKiEPj/9VqI9i"
